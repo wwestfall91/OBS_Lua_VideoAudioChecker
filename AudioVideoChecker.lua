ce37eeab9887e252c29778eb96810bf5a0ed0563
Wwestfall91 <wwestfall91@gmail.com>
2023-11-20 21:19:59 -0500

    Add helper functions, cleaned up code

--- AudioVideoChecker.lua
+++ AudioVideoChecker.lua
@@ -1,17 +1,13 @@
 obs = obslua
 local icon = "put base64 data here"
 local description = "This is a description that can also contain html using [[<all html elements>]] if need be"
-
-function setup()
-    local properties = obs.obs_properties_create()
-    local runButton = obs.obs_properties_add_button(properties, "run_button", "Run", button_clicked)
-    local stopButton = obs.obs_properties_add_button(properties, "stop_button", "Stop", stop_clicked)
-    return properties
-end
-
+properties = {}
 --------SCRIPT------
 function script_properties()
-    setup()
+    properties = obs.obs_properties_create()
+    local runButton = createButton("Run", button_clicked)
+    local stopButton = createButton("Stop", stop_clicked)
+    return properties
 end
 
 function script_description()
@@ -28,11 +24,14 @@ function stopTimer()
 end
 
 function timerCallback()
-    obs.obs_data_get_string
+    --obs.obs_data_get_string
 end
 
 --------BUTTONS--------
 function button_clicked()
+    -- source = getSourceByName("Television")
+    -- local file = getFileFromMediaSource(source)
+    -- log(file)
     local source = getSourceByName("Television")
     local settings = obs.obs_source_get_settings(source);
     obs.obs_data_set_string(settings, "local_file", "D:/YouTube/60 Second Gaming/Final Fantasy Told In/FF7.mp4")
@@ -47,6 +46,18 @@ function stop_clicked()
 end
 
 --------OBS HELPERS---------
+function createButton(label, click_function)
+    local buttonName = string.format("%s_button", label)
+    return obs.obs_properties_add_button(properties, buttonName, label, click_function)
+end
+
+function getFileFromMediaSource(source)
+    local settings = obs.obs_source_get_settings(source);
+    local fileString = obs.obs_data_get_string(settings, "local_file")
+    obs.obs_data_release(settings)
+    return fileString
+end
+
 function getSourceByName(sourceName)
     local source = obs.obs_get_source_by_name(sourceName)
     return source
